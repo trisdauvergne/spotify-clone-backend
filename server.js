44883509a743e8a2df4cd7a8d7e250f2894caac4
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-07-26 13:31:33 +0200

    Reverted some changes to test in development

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-// const redirect = app.settings.env === 'development' ? 'http://localhost:3000' :'https://trisdauvergne-spotify-clone.netlify.app/';
+const redirect = app.settings.env === 'development' ? 'http://localhost:3000' :'https://trisdauvergne-spotify-clone.netlify.app/';
 
 const credentials = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: 'https://trisdauvergne-spotify-clone.netlify.app/',
+  redirectUri: redirect,
 }
 
 console.log('in server.js line 14', app.settings.env);
@@ -21,11 +21,8 @@ console.log('In server.js', credentials);
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// // use the express-static middlewares
-// app.use(express.static('public'))
 
 app.get('/lyrics', async (req, res) => {
-  console.log('************* lyrics', req);
   const lyrics = await lyricsFinder(req.query.artist, req.query.track) || 'No lyrics found';
   res.json({ lyrics });
 });
